86d039224905486c364f45d7ad10ee40c93e70df
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-12-30 17:08:53 +0100

    disabled buttons at the end of the game

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,13 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button>{{ value }}</button>
+      <button [disabled]="disabled">{{ value }}</button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
   @Input() value: 'X' | 'O';
+
+  @Input() disabled: boolean = false;
 
 
 }
